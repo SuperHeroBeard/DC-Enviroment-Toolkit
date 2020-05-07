220d704e99e6021c7f38855bf6766a6ef05f4ca0
Andrew Johnson <andrew1994android@gmail.com>
2020-05-07 11:31:56 +0100

    Added additional values, Gathering Domain information is done and working on Gathering DHCP information

--- DC Enviroment Toolkit.ps1
+++ DC Enviroment Toolkit.ps1
@@ -4,16 +4,25 @@ function Show-Menu {
         [String]$Title = 'DC Enviroment Toolkit')
         Clear-Host
         Write-Host "========== $Title =========="
-        Write-Host "========== $Info =========="
         Write-Host "1: Press "1" to Gather Domain information"
-
+        Write-Host "2: Press "2" to Gather DHCP Settings"
+        Write-Host "Q: Press "Q" to Quit" 
     }
 
     function GatherDCInfo {
-        
-        
+        #Gathers Information on the forest mode and how many domains exist
+        Get-ADForest | Select-Object DomainNamingMaster, Domains,ForestMode,Name,RootDomain
+        #Gathers Information on fsmo roles
+        Get-ADDomain | Select-Object InfrastructureMaster, RIDMaster, PDCEmulator
+        Get-ADForest | Select-Object DomainNamingMaster, SchemaMaster   
     }
 
+    function GatherDHCPInfo {
+        $ScopeId = (Get-DhcpServerv4Scope | Select-Object ScopeId)
+        Get-DhcpServerv4Scope | Select-Object ScopeId,SubnetMask,StartRange,EndRange | Format-List
+        Get-DhcpServerv4Reservation -ScopeId $ScopeId 
+
+    }
 
 
     do 
@@ -24,6 +33,9 @@ function Show-Menu {
     {
         '1'{
             GatherDCInfo
+            }
+        '2'{
+            GatherDHCPInfo
             }
         'Q' {
                 return      
